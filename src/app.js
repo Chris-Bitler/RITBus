ed8a55e580f6728aad7f5b99dc3dc9ffdd040062
VoidWhisperer2 <voidwhisperer@voidwhisperer.info>
2016-03-14 14:39:13 -0400

    Fix stop referencing the wrong stops.

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ ajax(
     //console.log("test1");
     for(var i = 0; i < data.data.length; i++) {
       var stop = data.data[i];
-      console.log(stop.stop_id);
+      //console.log(stop.stop_id);
       stops[stop.stop_id] = stop;
     }
   },
@@ -90,17 +90,6 @@ function getRouteByName(title) {
   return route;
 }
 
-function getStopByName(title) {
-  var stop;
-  stops.forEach(function(element,index,array) {
-   // console.log(routes[index].long_name + " - " + title);
-    if(stops[index].name === title) {
-      stop = stops[index];
-    }
-  });
-  return stop;
-}
-
 function createMenu(items) {
   mainMenu = new UI.Menu({
       sections: [{
@@ -114,7 +103,7 @@ function createMenu(items) {
     splashWindow.hide();
 }
 
-function createMenu2(route, items) {
+function createMenu2(route, items, stopRef) {
   menu2 = new UI.Menu({
       sections: [{
         title: 'Bus Stops - ' + route.long_name,
@@ -122,7 +111,12 @@ function createMenu2(route, items) {
       }]
     });
     curRouteStop = route;
-    menu2.on('select', stopSelect);
+    menu2.on('select', function(e) {
+      var rid = curRouteStop.route_id;
+      var stop = stops[stopRef[e.itemIndex]];
+      //consoe.log('https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=643&callback=call&routes=' + rid + '&stops=' + stop.stop_id);
+      showStop(rid,stop);
+    });
   
     menu2.show();
     //mainMenu.hide();
@@ -132,20 +126,17 @@ function routeSelect(e) {
   var route = getRouteByName(e.item.title);
   if(route !== undefined) {
     var items = [];
+    var stopRef = [];
+    var i = 0;
     route.stops.forEach(function(element, index, array) {
       if(stops[route.stops[index]] !== undefined) {
         items.push({title: stops[route.stops[index]].name});
+        stopRef[i] = route.stops[index];
+        i++;
       }
     });
-    createMenu2(route,items);
+    createMenu2(route,items,stopRef);
   }
-}
-
-function stopSelect(e) {
-  var rid = curRouteStop.route_id;
-  var stop = getStopByName(e.item.title);
-  console.log('https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=643&callback=call&routes=' + rid + '&stops=' + stop.stop_id);
-  showStop(rid,stop);
 }
 
 function showStop(rid,stop) {
@@ -173,14 +164,14 @@ function showStop(rid,stop) {
           });
           stopData.show();
           accel.on("tap", function() {
-            console.log("Refreshing");
+            //console.log("Refreshing");
               ajax(
                 {
                   url:'https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=643&callback=call&routes=' + rid + '&stops=' + stop.stop_id,
                   headers: {"X-Mashape-Key" : "vuDPS5EJPVmshPRON0u2wZK9Rqdap1Asa8sjsn3GevvSArSsaj"},
                   type:'json'
                 },function(data) {
-                  console.log("set body");
+                  //console.log("set body");
                   stopData.body("Updating..");
                   if(data.data.length !== 0) {
                     if(data.data[0].arrivals.length !== 0) {
@@ -189,14 +180,14 @@ function showStop(rid,stop) {
                         var diff = millisStop - now;
                         var min = Math.floor((diff/1000)/60);
                       stopData.body("Arriving in approximately " + min + " minutes! Shake to refresh.");
-                      console.log("Setbody 2");
+                     // console.log("Setbody 2");
                     }else{
                       stopData.body("Error retrieving data. Shake to try again.");
-                      console.log("Setbody 3");
+                    //  console.log("Setbody 3");
                     }
                   }else{
                     stopData.body("Error retrieving data. Shake to try again.");
-                    console.log("Setbody 4")
+                    //console.log("Setbody 4")
                   }
                 },function(error){
                   
